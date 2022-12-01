b76e1e5ca142f5de47d0c84aa1a857c22f401c6d
Pedro <pedro.teixeiravila@gmail.com>
2022-12-01 02:05:26 -0300

    cria barra

---
+++ pbarra.py
@@ -0,0 +1,18 @@
+import pygame
+PRETO = (0,0,0)
+
+#cria a classe barra 
+class Barra(pygame.sprite.Sprite):
+
+ 
+    def __init__(self, color, width, height):
+        super().__init__()
+ 
+        # altura e largura da barra
+        self.image = pygame.Surface([width, height])
+        self.image.fill(PRETO)
+        self.image.set_colorkey(PRETO)
+ 
+        # Desenha a barra
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
